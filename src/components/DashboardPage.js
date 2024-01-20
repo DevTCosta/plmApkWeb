4d2b2c3abe15ebc66b16ee4e7ca514f68cf9dcf7
Tiago Costa <tcperez@cea.plm.pt>
2024-01-20 13:12:08 +0000

    atualizacao para integracao com backend

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -7,18 +7,44 @@ import Header from './Header';
 import '../styles/DashboardPage.css';
 import '../styles/Fonts.css';
 
-
-
 const DashboardPage = ({ loggedIn, username }) => {
   const [backgroundColorbar, setBackgroundColor] = useState('');
-
+  const [mensagem, setMensagem] = useState('');
+  const [videos, setVideos] = useState([]);
+  
   useEffect(() => {
-    if (loggedIn) {
-      setBackgroundColor(getBgcolorBar(username));
-    } else {
-      setBackgroundColor('');
-    }
+      const fetchData = async () => {
+          try {
+              if (loggedIn) {
+                  const [mensagemResponse, videosResponse] = await Promise.all([
+                      fetch('http://localhost:3001/api/mensagem'),
+                      fetch('http://localhost:3001/api/videos/nomes-dos-videos')
+                  ]);
+  
+                  if (!mensagemResponse.ok) {
+                      throw new Error(`Erro ao buscar mensagem. Status: ${mensagemResponse.status}`);
+                  }
+                  const mensagemData = await mensagemResponse.json();
+                  setMensagem(mensagemData.mensagem);
+  
+                  if (!videosResponse.ok) {
+                      throw new Error(`Erro ao buscar informações dos vídeos. Status: ${videosResponse.status}`);
+                  }
+                  const videosData = await videosResponse.json();
+                  setVideos(videosData.videos);
+  
+                  setBackgroundColor(getBgcolorBar(username));
+              } else {
+                  setBackgroundColor('');
+              }
+          } catch (error) {
+              console.error('Erro ao buscar informações:', error.message);
+          }
+      };
+  
+      fetchData();
   }, [loggedIn, username]);
+  
 
   const getBgcolorBar = (username) => {
     if (username === 'widex') {
@@ -42,6 +68,16 @@ const DashboardPage = ({ loggedIn, username }) => {
               <img className='trashIcon' src={trashIcon} alt='trashIcon' />
             </div>
           </div>
+          <div className='ingoGeneralContainer'>
+                <div>
+                  <p>Mensagem do servidor:</p>
+                  <p>{mensagem}</p>
+                </div>
+                <div>
+                  <p>Total de vídeos:</p>
+                  <p>{videos}</p>
+                </div>
+          </div>
         </div>
         <div className="lojaHistoryContainer" >
           <div className='lojaContainer'>
@@ -55,7 +91,7 @@ const DashboardPage = ({ loggedIn, username }) => {
           </div>
           <div className='historyCotainer'>
           <div className="generalBar" style={{ backgroundColor: backgroundColorbar }}>
-            <p className="txtGeneralBar">Historico</p>
+            <p className="txtGeneralBar">Histórico</p>
             <div class="iconsGeneral">
               <img className='uploadIcon' src={uploadIcon} alt='uploadIcon' />
               <img className='trashIcon' src={trashIcon} alt='trashIcon' />
